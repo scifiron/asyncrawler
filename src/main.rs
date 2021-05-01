93974baa9dedeada2cf74b625dcc915d1a3d9166
Scifiron <scifiron@protonmail.com>
2021-05-01 17:16:00 -0300

    Single request.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+use reqwest::Client;
+
+#[tokio::main(flavor = "current_thread")]
+async fn main() -> Result<(), String> {
+    let url = get_url()?;
+    let body = make_request(&url).await?;
+    println!("{}", body);
+    Ok(())
+}
+
+fn get_url() -> Result<String, String> {
+    env::args().nth(1)
+        .ok_or(String::from("Url should be passed as argument"))
+}
+
+async fn make_request(url: &str) -> Result<String, String> {
+    let client = Client::new();
+
+    let response = client.get(url)
+        .send().await
+        .map_err(|err| err.to_string())?;
+
+    let body = response.text()
+        .await
+        .map_err(|err| err.to_string())?;
+
+    Ok(body)
 }
