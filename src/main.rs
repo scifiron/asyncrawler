07527c899d591dc7758b53b46f1c554fbdf10b27
Scifiron <scifiron@protonmail.com>
2021-05-02 10:11:44 -0300

    Normalizing urls.

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,16 @@
 use std::env;
-use reqwest::Client;
+use reqwest::{Client, Url};
 use select::document::Document;
 use select::predicate::Name;
+use url::ParseError;
 
 #[tokio::main(flavor = "current_thread")]
 async fn main() -> Result<(), String> {
     let url = get_url()?;
-    let body = make_request(&url).await?;
+    let url = Url::parse(&url).map_err(|err| err.to_string())?;
+    let body = make_request(url.clone()).await?;
 
-    for url in get_urls(&body)? {
+    for url in get_urls(&body, &url)? {
         println!("{}", url);
     }
 
@@ -20,7 +22,7 @@ fn get_url() -> Result<String, String> {
         .ok_or(String::from("Url should be passed as argument"))
 }
 
-async fn make_request(url: &str) -> Result<String, String> {
+async fn make_request(url: Url) -> Result<String, String> {
     let client = Client::new();
 
     let response = client.get(url)
@@ -34,15 +36,26 @@ async fn make_request(url: &str) -> Result<String, String> {
     Ok(body)
 }
 
-fn get_urls(body: &str) -> Result<Vec<String>, String> {
+fn get_urls(body: &str, source_url: &Url) -> Result<Vec<Url>, String> {
     let document = Document::from_read(body.as_bytes())
         .map_err(|err| err.to_string())?;
 
     let urls = document.find(Name("a"))
-        .filter_map(|node| node.attr("href").map(|x| x.to_string()))
+        .filter_map(|node| node.attr("href"))
+        .filter_map(|url| normalize_url(source_url, url))
         .collect();
 
     Ok(urls)
+}
+
+fn normalize_url(source_url: &Url, url: &str) -> Option<Url> {
+    match Url::parse(url) {
+        Ok(url) => Some(url),
+        Err(ParseError::RelativeUrlWithoutBase) => {
+            source_url.join(url).ok()
+        }
+        _ => None
+    }
 }
 
 
